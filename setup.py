d466ca27d31ad11c39a42b0809c7e60b1510b369
Piergiacomo De Marchi <piergiacomo.demarchi@gmail.com>
2024-06-11 19:31:38 +0200

    Supporting Python 3.10

--- setup.py
+++ setup.py
@@ -18,13 +18,15 @@ test_requirements = [
 setup(
     author="Lynxius Inc.",
     author_email="contact@lynxius.ai",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.12",
     ],
     description="Lynxius speeds up LLM evaluation and prevents errors before they reach users.",
@@ -43,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lynxius/lynxius-python",
-    version="0.1.2",
+    version="1.0.0",
     zip_safe=False,
 )
